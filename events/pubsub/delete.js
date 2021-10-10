d1d0b01a1ab2b2cc98c53fc2cd4e47ce13d9e102
Corentin <c.faure1996@gmail.com>
2021-10-10 14:10:58 +0200

    Ajout event delete

--- events/pubsub/delete.js
+++ events/pubsub/delete.js
@@ -2,25 +2,25 @@
   * @INFO
   * Loading all needed File Information Parameters
 */
-
-const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
 const { MessageEmbed } = require("discord.js");
+const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
 
 //here the event starts
 module.exports = async (client, twitch, pubsub, data) => {
   const logsModerationTwitchDiscordChannel = client.channels.cache.get("629284733653745664");
   try {
     return logsModerationTwitchDiscordChannel.send(new MessageEmbed()
-      .setColor(ee.color)
+      .setColor(ee.wrongcolor)
       .setFooter(ee.footertext, ee.footericon)
-      .setTitle(`:angel: UNBAN :angel:`)
-      .setDescription(data.target.name + " a été unban par " + data.moderator.name + "\n https://www.twitch.tv/popout/cruelladk/viewercard/" + data.target.name)
+      .setTitle(`:no_entry_sign: DELETE :no_entry_sign:`)
+      .setDescription("Message de " + data.target.name + " delete par " + data.moderator.name + "\n Message : " + data.message.content + "\n https://www.twitch.tv/popout/cruelladk/viewercard/" + data.target.name)
     );
+
   } catch (e) {
     return logsModerationTwitchDiscordChannel.send(
       new MessageEmbed()
         .setColor(ee.wrongcolor)
-        .setTitle(`❌ ERREUR | Une erreur est survenue avec l'event d'unban.`)
+        .setTitle(`❌ ERREUR | Une erreur est survenue avec l'event de ban`)
         .setDescription(`\`\`\`${e.stack}\`\`\``)
     );
   }
